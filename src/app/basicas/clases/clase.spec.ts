d8280c032c114c3140f7e1bf2236ae4612cb4662
jimmysn <jimmyr.salazar@gmail.com>
2020-10-12 19:12:28 -0500

    test - classes (2) - Ciclo de vida pruebas

--- src/app/basicas/clases/clase.spec.ts
+++ src/app/basicas/clases/clase.spec.ts
@@ -3,14 +3,24 @@ import { Jugador } from './clase';
 
 describe('Pruebas de clase', ()=>{
 
-    const jugador = new Jugador();
+    let jugador = new Jugador();
 
     // CICLO DE VIDA DE LAS PRUEBAS
-    // beforeAll();
-    // beforeEach();
+    beforeAll(()=>{
+        // console.log('beforeAll');
+    });
+    beforeEach(()=>{
+        // console.log('beforeEach');
+        //jugador.hp = 100;
+        jugador = new Jugador();
+    });
 
-    // afterAll();
-    // afterEach();
+    afterAll(()=>{
+        // console.log('afterAll');
+    });
+    afterEach(()=>{
+        // console.log('afterEach');
+    });
     
     it('Debe de retornar 80 de hp, si recibe 20 de daño', ()=>{
         
@@ -25,6 +35,13 @@ describe('Pruebas de clase', ()=>{
         // const jugador = new Jugador();
         const resp = jugador.recibeDanio(50);
         expect(resp).toBe(50);
+
+    });
+
+    it('Debe de retornar 0 de hp, si recibe 100 de daño', ()=>{
+        
+        const resp = jugador.recibeDanio(100);
+        expect(resp).toBe(0);
 
     });
 
